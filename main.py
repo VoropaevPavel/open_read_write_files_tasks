29f898eb650ff770004e614477519db0f95793d4
Pavel Voropaev <paulvoropaev@gmail.com>
2023-08-13 19:23:58 +0300

    Task 2 is done

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ with open('recipes.txt', encoding="utf-8") as f:
             d.append({'ingredient_name': ingredient_name, 'quantity': quantity, 'measure': measure})
         cook_book[name] = d
 
-print(f'cook_book = {cook_book}')
+# print(f'cook_book = {cook_book}')
 
-# result = {}
-# def get_shop_list_by_dishes(dishes, person_count):
-#     for dish in dishes:
-#         if dish in cook_book:
-#             for ingredients in cook_book[dish]:
-#                 if ingredients['ingredient_name'] in result:
-#                     result[ingredients['ingredient_name']]['quantity'] += int(ingredients['quantity']) * person_count
-#                 else:
-#                     result[ingredients['ingredient_name']] = {'measure': ingredients['measure'], 'quantity': int(ingredients['quantity']) * person_count}
-#
-# get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
-# print(result)
+result = {}
+def get_shop_list_by_dishes(dishes, person_count):
+    for dish in dishes:
+        if dish in cook_book:
+            for ingredients in cook_book[dish]:
+                if ingredients['ingredient_name'] in result:
+                    result[ingredients['ingredient_name']]['quantity'] += int(ingredients['quantity']) * person_count
+                else:
+                    result[ingredients['ingredient_name']] = {'measure': ingredients['measure'], 'quantity': int(ingredients['quantity']) * person_count}
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+print(result)
 
